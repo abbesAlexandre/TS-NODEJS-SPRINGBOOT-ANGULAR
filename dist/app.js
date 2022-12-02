301fca704de650028e61f3d4aeb2640a1935cbd9
KimiWaDare <abbes.alexandre@gmail.com>
2022-12-02 20:31:34 +0100

    mise en place typescript

---
+++ dist/app.js
@@ -0,0 +1,7 @@
+const compteur = document.querySelector('#compteur');
+let i = 0;
+const increment = (e) => {
+    i++;
+    compteur.querySelector('span').innerText = i.toString();
+};
+compteur.addEventListener('click', increment);
