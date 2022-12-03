8ba23789073090b9ae9155c0dea162ebdc2081e2
KimiWaDare <abbes.alexandre@gmail.com>
2022-12-03 01:03:20 +0100

    typescrit first app

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,35 @@
+"use strict";
+/** *
+const a = "firstname"
+const n = 3
+const b = true
+const d = null
+const arr = ['aze','aze', 'aze']
+const user = {firstname:"Alexandre", lastname:"Abbes"}
+const date = new Date();
+const cb: (e: MouseEvent) => void = (e:MouseEvent): number => {
+    return 3
+}
+const compteur = document.querySelector('#compteur') as HTMLButtonElement
+function printId(id: number | string): void{
+    console.log(id.toString());
+}
+*/
 const compteur = document.querySelector('#compteur');
 let i = 0;
 const increment = (e) => {
     i++;
-    compteur.querySelector('span').innerText = i.toString();
+    const span = compteur === null || compteur === void 0 ? void 0 : compteur.querySelector('span');
+    if (span) {
+        span.innerText = i.toString();
+    }
 };
-compteur.addEventListener('click', increment);
+
+function printId(id) {
+    if (typeof id == "number") {
+        console.log((id * 3).toString());
+    } else {
+        console.log(id.toUpperCase());
+    }
+}
+compteur === null || compteur === void 0 ? void 0 : compteur.addEventListener('click', increment);
