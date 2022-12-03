657eeb3008a3b54d11a927b308942e38e3f8f3d2
KimiWaDare <abbes.alexandre@gmail.com>
2022-12-03 15:05:17 +0100

    Epuration du code

--- Typescript/dist/app.js
+++ Typescript/dist/app.js
@@ -1,25 +1,9 @@
 "use strict";
-/** *
-const a = "firstname"
-const n = 3
-const b = true
-const d = null
-const arr = ['aze','aze', 'aze']
-const user = {firstname:"Alexandre", lastname:"Abbes"}
-const date = new Date();
-const cb: (e: MouseEvent) => void = (e:MouseEvent): number => {
-    return 3
-}
-const compteur = document.querySelector('#compteur') as HTMLButtonElement
-function printId(id: number | string): void{
-    console.log(id.toString());
-}
-*/
 const compteur = document.querySelector('#compteur');
 let i = 0;
 const increment = (e) => {
     i++;
-    const span = compteur === null || compteur === void 0 ? void 0 : compteur.querySelector('span');
+    const span = compteur?.querySelector('span');
     if (span) {
         span.innerText = i.toString();
     }
@@ -32,4 +16,4 @@ function printId(id) {
         console.log(id.toUpperCase());
     }
 }
-compteur === null || compteur === void 0 ? void 0 : compteur.addEventListener('click', increment);
+compteur?.addEventListener('click', increment);
--- Typescript/src/app.ts
+++ Typescript/src/app.ts
@@ -1,20 +1,3 @@
-/** *
-const a = "firstname"
-const n = 3
-const b = true
-const d = null
-const arr = ['aze','aze', 'aze']
-const user = {firstname:"Alexandre", lastname:"Abbes"}
-const date = new Date();
-const cb: (e: MouseEvent) => void = (e:MouseEvent): number => {
-    return 3
-}
-const compteur = document.querySelector('#compteur') as HTMLButtonElement
-function printId(id: number | string): void{
-    console.log(id.toString());
-}
-*/
-
 const compteur = document.querySelector('#compteur')
 
 let i = 0;
@@ -22,14 +5,14 @@ let i = 0;
 const increment = (e: Event) => {
     i++;
     const span = compteur?.querySelector('span')
-    if (span){
+    if (span) {
         span.innerText = i.toString();
     }
 }
-function printId(id: number | string){
-    if (typeof id == "number" ){
+function printId(id: number | string) {
+    if (typeof id == "number") {
         console.log((id * 3).toString());
-    }else{
+    } else {
         console.log(id.toUpperCase())
     }
 }
