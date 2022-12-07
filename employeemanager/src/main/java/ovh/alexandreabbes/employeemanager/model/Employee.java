e10708b95ffc679a65824a225844a07a22270172
KimiWaDare <abbes.alexandre@gmail.com>
2022-12-07 16:51:37 +0100

    Mise en place springboot tutoriel employee

---
+++ employeemanager/src/main/java/ovh/alexandreabbes/employeemanager/model/Employee.java
@@ -0,0 +1,11 @@
+package ovh.alexandreabbes.employeemanager.model;
+
+public class Employee {
+    private Long id;
+    private String  name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl; // will hold the location where we can read the img
+    private String employeeCode;
+}
