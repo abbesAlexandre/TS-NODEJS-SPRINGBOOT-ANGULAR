a66d36d043921cce5d8399fa787bcb411ce3e6f2
KimiWaDare <abbes.alexandre@gmail.com>
2022-12-10 13:09:06 +0100

    Service class  + exception handling

---
+++ employeemanager/src/main/java/ovh/alexandreabbes/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package ovh.alexandreabbes.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String message){
+        super(message);
+    }
+}
